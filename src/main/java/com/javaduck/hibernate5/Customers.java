3d9f08989b59cc3d77f1058f94adee9aaca6fa84
Debashis Nandi <37268150+debashisz@users.noreply.github.com>
2018-03-29 07:24:33 +0530

    another comment

--- src/main/java/com/javaduck/hibernate5/Customers.java
+++ src/main/java/com/javaduck/hibernate5/Customers.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.javaduck.hibernate5;
+//again another message
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Basic;
